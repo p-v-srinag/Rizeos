e0de0dda4c17101b564c1b56b0d19d0bfa0b7377
p-v-srinag <pvenkatasrinag@gmail.com>
2025-08-20 21:06:18 +0530

    c20

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI;
